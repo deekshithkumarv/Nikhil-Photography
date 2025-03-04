47e7f332205ee09afe16d2f68d7e2cdd27ea836f
Deekshith Kumar V <99490496+deekshithkumarv@users.noreply.github.com>
2025-03-04 18:41:41 +0530

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-[1] mx-auto flex w-full flex-wrap items-center justify-between border-b border-gray-100 bg-background p-[1em] font-sans font-semibold uppercase text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary">
       {/* <Logo /> */}
-      <span className="font-mono tracking-tight text-lg">Nikhil</span>
+      <span className="font-mono tracking-tight text-lg">Nikhil Studio</span>
       <Nav />
     </header>
   );
